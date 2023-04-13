944a27cffed7a7059df9674ac34f83c628ff960a
James Reed <james@twiddlingbits.net>
2023-04-13 14:40:19 -0400

    backend: Remove TA strategy subclass

--- backend/src/lib/strategies/engine.js
+++ backend/src/lib/strategies/engine.js
@@ -1,6 +1,8 @@
-import { StrategyEngine } from "../strategy.js"
+import { StrategyEngine, TAStrategy } from "../strategy.js"
 
 import Consensus from "./consensus.js"
-import TASimple from "./ta_simple.js"
 
-export default new StrategyEngine([new TASimple(), new Consensus()])
+export default new StrategyEngine([
+  new TAStrategy("ta_simple", "simple"),
+  new Consensus(),
+])
