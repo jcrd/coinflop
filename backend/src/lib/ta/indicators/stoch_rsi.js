993d4f302211e86147138adba1cc630810622847
James Reed <james@twiddlingbits.net>
2023-04-13 15:26:27 -0400

    backend: Move SMA into Stoch RSI class

--- backend/src/lib/ta/indicators/stoch_rsi.js
+++ backend/src/lib/ta/indicators/stoch_rsi.js
@@ -1,4 +1,4 @@
-import { RSI } from "@debut/indicators"
+import { RSI, SMA } from "@debut/indicators"
 
 import Period from "../period.js"
 
@@ -6,6 +6,7 @@ export class StochRSI {
   constructor(period = 14) {
     this.period = new Period(period)
     this.rsi = new RSI(period)
+    this.sma = new SMA(3)
   }
   nextValue(value) {
     const rsi = this.rsi.nextValue(value)
@@ -13,8 +14,12 @@ export class StochRSI {
       const lowest = this.period.lowest()
       const highest = this.period.highest()
       const denom = highest - lowest
-      const v = denom == 0 ? 1.0 : (rsi - lowest) / denom
-      return v
+      const k = denom == 0 ? 1.0 : (rsi - lowest) / denom
+      const d = this.sma.nextValue(k)
+      if (d === undefined) {
+        return undefined
+      }
+      return { k, d }
     }
     return undefined
   }
